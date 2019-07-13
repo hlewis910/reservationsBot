0317b3ee85285284f429c3ed9ddeea48bc722d4e
Sola <solaocodes@gmai.com>
2019-07-13 19:50:38 -0400

    Add ability to send and receive messages on the reservations POST route.

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-/* GET reservations listing. */
-router.get('/', function(req, res, next) {
-  res.json([
-    {id: 1, name: "Chris, party of 2 @ 8pm"},
-    {id: 2, name: "Edward, party of 5 @ 2pm"}
-  ]);
+/* POST create reservations */
+router.post('/', function(req, res, next) {
+  var twiml = new MessagingResponse();
+
+  // The content of the text message sent to twilio.
+  console.log(req.body.Body)
+
+  //  The message we send back after we make a reservation (successfully or
+  //  otherwise).
+  twiml.message("We'll have your reservations details very soon!");
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 module.exports = router;

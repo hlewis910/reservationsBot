5cc0a5f411857d28433f74a663beb91d0ff4a766
Hariet <harietlewis@gmail.com>
2019-07-15 16:32:47 -0400

    Text updates

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+const reservationData = [];
+
+/* GET request */
+router.get('/', function(req, res, next) {
+  res.json(
+    reservationData
+  )
+})
+
 /* POST create reservations */
 router.post('/', function(req, res, next) {
   var twiml = new MessagingResponse();
@@ -9,7 +18,8 @@ router.post('/', function(req, res, next) {
   // The content of the text message sent to twilio.
   var reservationMessage = req.body.Body.split('/');
   var reservationDate = new Date(reservationMessage[2]);
-  var reservationTime = reservationDate.getHours();
+  var reservationHours = reservationDate.getHours();
+  var reservationMin =  (reservationDate.getMinutes() < 10) ? '0' + reservationDate.getMinutes() : reservationDate.getMinutes()
 
   //  The message we send back after we make a reservation (successfully or
   //  otherwise).
@@ -23,8 +33,17 @@ router.post('/', function(req, res, next) {
   };
 
 
-  if (restaurant.opening < reservationTime && reservationTime < restaurant.closing) {
+  if (restaurant.opening < reservationHours && reservationHours < restaurant.closing - 1) {
     message = 'Reservation successful';
+    const reservationId = reservationData.length + 1;
+    reservationData.push({
+      id: reservationId,
+      name: `${reservationMessage[0]}
+            ${reservationMessage[1]} at
+            ${reservationHours}:${reservationMin}`
+    });
+
+
   } else {
     message = 'Reservation unsucessful';
   }

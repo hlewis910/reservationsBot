e425249840c4e8dab79a408a3a5a1c2eaafd68e0
Hariet <harietlewis@gmail.com>
2019-07-15 15:44:31 -0400

    Reservation Success Rate

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -7,11 +7,30 @@ router.post('/', function(req, res, next) {
   var twiml = new MessagingResponse();
 
   // The content of the text message sent to twilio.
-  console.log(req.body.Body)
+  var reservationMessage = req.body.Body.split('/');
+  var reservationDate = new Date(reservationMessage[2]);
+  var reservationTime = reservationDate.getHours();
 
   //  The message we send back after we make a reservation (successfully or
   //  otherwise).
-  twiml.message("We'll have your reservations details very soon!");
+
+  var message = '';
+
+  const restaurant = {
+    opening: 8,
+    closing: 22,
+    name: 'Hariet'
+  };
+
+
+  if (restaurant.opening < reservationTime && reservationTime < restaurant.closing) {
+    message = 'Reservation successful';
+  } else {
+    message = 'Reservation unsucessful';
+  }
+
+  twiml.message(message);
+
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
